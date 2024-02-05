e082ea6f66c97d4fb6b220b0e50b4c142ea72339
enver-isliamov <enverphoto@gmail.com>
2024-02-06 00:48:10 +0300

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 # Токен вашего бота
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = "6473374979:AAH8OHCxWN2kO0ep9wrbLXolk2ys4__GLqg"
 
 # Обработчик команды /start
 def start(update: Update, context: CallbackContext) -> None:
@@ -10,14 +10,14 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(
         f"Привет, {user.first_name}! Выбери действие:",
         reply_markup={
-            "keyboard": [["Кнопка 1", "Кнопка 2"], ["Кнопка 3"]],
+            "keyboard": [["Калькулятор", "Оставить заявку"], ["Кнопка 3"]],
             "one_time_keyboard": True,
         },
     )
 
 # Главная функция
 def main() -> None:
-    updater = Updater(6473374979:AAH8OHCxWN2kO0ep9wrbLXolk2ys4__GLqg)
+    updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
